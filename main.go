3aec36f285ab699d53a73c14ec86929d735c66f5
staf3333 <staf3333@gmail.com>
2024-01-21 12:38:28 -0800

    refactor main.go logic

--- main.go
+++ main.go
@@ -9,26 +9,11 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
 	"github.com/staf3333/chirpy/internal/database"
 )
-
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
 
 // create function that matches this signature
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,14 +27,6 @@ type apiConfig struct {
 	fileServerHits int
 	db *database.DB
 	jwtSecret string
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileServerHits += 1
-		w.Header().Set("Cache-Control", "No-Cache")
-		next.ServeHTTP(w, r)
-	})
 }
 
 func outputMetricsHtml(w http.ResponseWriter, filename string, data interface{}) {
@@ -191,165 +168,8 @@ func (cfg *apiConfig) chirpsWithIDHandler(w http.ResponseWriter, r *http.Request
 
 }
 
-func (cfg *apiConfig) userCreateHandler(w http.ResponseWriter, r *http.Request) {
-
-	defer r.Body.Close()
-	type requestBody struct {
-		Email string `json:"email"`
-		Password string `json:"password"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := requestBody{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		log.Printf("Error decoding JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-
-	user, err := cfg.db.CreateUser(params.Email, params.Password)	
-	if err != nil {
-		// respondWithError(w, 500, "Error creating user in DB")
-		respondWithError(w, 500, err.Error())
-		return
-	}
-	respondWithJSON(w, 201, struct {
-		Email string `json:"email"`
-		ID int `json:"id"`
-	}{
-		Email: user.Email,
-		ID: user.ID,
-	})
-}
-
 func stripAuthHeaderPrefix(h string) string {
 	return strings.Split(h, " ")[1]
-}
-
-func (cfg *apiConfig) userUpdateHandler(w http.ResponseWriter, r *http.Request) {
-	
-	defer r.Body.Close()
-	type requestBody struct {
-		Email string `json:"email"`
-		Password string `json:"password"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := requestBody{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		log.Printf("Error decoding JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-
-	authHeader := r.Header.Get("Authorization")
-	var tokenString string
-	if len(authHeader) == 0 {
-		log.Printf("unauthorized attempt to modify resources")
-		w.WriteHeader(401)
-		return
-	}
-	tokenString = stripAuthHeaderPrefix(authHeader)
-
-	type MyCustomClaims struct {
-		jwt.RegisteredClaims
-	}
-	claims := &MyCustomClaims{}
-	keyFunc := func(token *jwt.Token) (interface{}, error) {
-		return []byte(cfg.jwtSecret), nil
-	}
-	token , err := jwt.ParseWithClaims(tokenString, claims, keyFunc)
-	if err != nil {
-		log.Printf("unauthorized attempt to modify resources, claims don't match")
-		respondWithError(w, 401, err.Error())
-		return
-	}
-
-	if !token.Valid {
-		log.Printf("token has expired")
-		respondWithError(w, 401, err.Error())
-		return
-	}
-	
-	userIDStr, err := token.Claims.GetSubject()
-	if err != nil {
-		log.Printf("issue getting subject from token")
-	}
-	userID, err := strconv.Atoi(userIDStr)
-	if err != nil {
-		log.Printf("Error converting id from string to int")
-	}
-
-	updatedUser, err := cfg.db.UpdateUser(userID, params.Email, params.Password)
-	if err != nil {
-		log.Printf("error updating user in the database")
-		w.WriteHeader(500)
-		return
-	}
-
-	respondWithJSON(w, 200, struct {
-		Email string `json:"email"`
-		ID int `json:"id"`
-	}{
-		Email: updatedUser.Email,
-		ID: updatedUser.ID,
-	})
-}
-
-func (cfg *apiConfig) loginHandler(w http.ResponseWriter, r *http.Request) {
-
-	defer r.Body.Close()
-	type requestBody struct {
-		Email string `json:"email"`
-		Password string `json:"password"`
-		Expiration *int `json:"expires_in_seconds,omitempty"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := requestBody{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		log.Printf("Error decoding JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-
-	user, err := cfg.db.LoginUser(params.Email, params.Password)
-	if err != nil {
-		respondWithError(w, 401, err.Error())
-		return
-	}
-
-	var expirationDuration time.Duration
-	if params.Expiration == nil || *params.Expiration > 86400 {
-		expirationDuration = time.Duration(86400) * time.Second
-	} else {
-		expirationDuration = time.Duration(*params.Expiration) * time.Second
-	}
-	jwt := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
-		Issuer: "chirpy",
-		IssuedAt: jwt.NewNumericDate(time.Now()),
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expirationDuration)),
-		Subject: strconv.Itoa(user.ID),
-	})
-
-	tokenString, err := jwt.SignedString([]byte(cfg.jwtSecret))
-	if err != nil {
-		fmt.Println(err)
-		log.Fatal("error generating users jwt token")
-	}
-
-	respondWithJSON(w, 200, struct {
-		Email string `json:"email"`
-		ID int `json:"id"`
-		Token string `json:"token"`
-	}{
-		Email: user.Email,
-		ID: user.ID,
-		Token: tokenString, 
-	})
 }
 
 func chirpsRoutes(cfg *apiConfig) *chi.Mux {
---
+++ middleware.go
@@ -0,0 +1,24 @@
+package main
+
+import "net/http"
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits += 1
+		w.Header().Set("Cache-Control", "No-Cache")
+		next.ServeHTTP(w, r)
+	})
+}
